3b176d1c95248cdf9ca6dd51e74e40af6a3d8bbf
GangPao <127927330+NavyCheng@users.noreply.github.com>
2023-08-16 23:02:50 +0800

    111
    111

--- _search.py
+++ _search.py
@@ -10,8 +10,8 @@ for i in range(0, 3499): #目标循环
     for j in range(0,89): #数据循环
         if df_Search_Data.iat[j,0] == df_Search_Target.iat[i,0]: #暴力匹配
             Data_Judge.append(df_Search_Data.iat[j,6]) #列表添加
-    max_value = max(Data_Judge,default=0) #对单目标列表计算极值
-    dic[df_Search_Target.iat[i,0]] = max_value #插入字典
+    min_value = min(Data_Judge,default=0) #对单目标列表计算极值
+    dic[df_Search_Target.iat[i,0]] = min_value #插入字典
 print(dic)
 
 
