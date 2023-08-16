a8d9a5caa90ca2dd40ebb83b7b4f04b0e4b413cf
WiFi1226 <138101033+WiFi1226@users.noreply.github.com>
2023-08-17 01:21:55 +0800

    字典的优化方法

---
+++ .history/_search_20230817003305.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+df_Search_Target = pd.read_excel(f"Target.xlsx",index_col=None) #引入数据目标
+df_Search_Data = pd.read_excel(f"Data",index_col=None) #引入匹配数据
+dic = {} #空列表
+df_Search_Data_Delete = list(range(0,len(df_Search_Data)-1))
+for i in range(0, len(df_Search_Target)): #目标循环
+    Data_Judge = [] #空列表
+    print(df_Search_Data_Delete)
+    for j in range(0,len(df_Search_Data_Delete)-1): #数据循环
+        print(j)
+        print("------")
+        print(len(df_Search_Data_Delete))
+        if df_Search_Data.iat[df_Search_Data_Delete[j]-1,0] == df_Search_Target.iat[i,0]: #暴力匹配
+            Data_Judge.append(df_Search_Data.iat[df_Search_Data_Delete[j],6]) #列表添加
+            print(Data_Judge)
+            del df_Search_Data_Delete[j]
+    min_value = min(Data_Judge,default=0) #对单目标列表计算极值
+    print(min_value)
+    dic[df_Search_Target.iat[i,0]] = min_value #插入字典
+print(dic)
+
+
+
+
+
